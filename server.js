f8a88ee48bb1342309d92375b59d1b5a265e63e1
Eyob-second <tupomaza43@gmail.com>
2026-03-25 20:12:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ wss.on("connection", (ws) => {
   console.log("New client connected");
 
   ws.on("message", (message) => {
-    console.log("Received:", message.toString());
+    alert("Received:", message.toString());
 
     // Broadcast to all clients
     clients.forEach(client => {
